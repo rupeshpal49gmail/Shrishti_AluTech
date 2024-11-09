bf945eff5ce2d589b1d4b159ac9197f5eb5d6e4f
Rupesh Pal <rupeshpal49@gmail.com>
2024-11-09 23:42:30 +0530

    add some slider with javascript

---
+++ HomePage/script.js
@@ -0,0 +1,41 @@
+function changeImage() {
+  document.getElementById("imgList").src =
+    "./images/Shrishti_AluTech/ST company-images-1.jpg";
+}
+function changeImage2() {
+  document.getElementById("imgList").src =
+    "./images/Shrishti_AluTech/ST company-images-2.jpg";
+}
+function changeImage3() {
+  document.getElementById("imgList").src =
+    "./images/Shrishti_AluTech/ST company-images-3.jpg";
+}
+function changeImage4() {
+  document.getElementById("imgList").src =
+    "./images/Shrishti_AluTech/ST company-images-4.jpg";
+}
+function changeImage5() {
+  document.getElementById("imgList").src =
+    "./images/Shrishti_AluTech/ST company-images-5.jpg";
+}
+function changeImage6() {
+  document.getElementById("imgList").src =
+    "./images/Shrishti_AluTech/ST company-images-6.jpg";
+}
+function changeImage7() {
+  document.getElementById("imgList").src =
+    "./images/Shrishti_AluTech/ST company-images-7.jpg";
+}
+function changeImage8() {
+  document.getElementById("imgList").src =
+    "./images/Shrishti_AluTech/ST company-images-8.jpg";
+}
+
+setInterval(changeImage, 2000);
+setInterval(changeImage2, 4000);
+setInterval(changeImage3, 6000);
+setInterval(changeImage4, 8000);
+setInterval(changeImage5, 10000);
+setInterval(changeImage6, 12000);
+setInterval(changeImage7, 14000);
+setInterval(changeImage8, 16000);
